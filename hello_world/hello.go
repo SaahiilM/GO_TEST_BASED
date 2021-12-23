4a2d2c5af926009245ad4388d536d18f43e2dc0f
SaahiilM <47355754+SaahiilM@users.noreply.github.com>
2021-12-23 08:25:38 -0500

    Made changes to accept empty string

--- hello_world/hello.go
+++ hello_world/hello.go
@@ -2,8 +2,13 @@ package main
 
 import "fmt"
 
+const prefixHello = "Hello, "
+
 func Hello(name string) string {
-	return "Hello, " + name
+	if name == "" {
+		name = "World"
+	}
+	return prefixHello + name
 }
 
 func main() {
