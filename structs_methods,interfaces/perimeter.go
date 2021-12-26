8a816e6930d1d2a9ab53f54ed512c92fd4aeb332
SaahiilM <47355754+SaahiilM@users.noreply.github.com>
2021-12-26 05:44:07 -0500

    introduced methods and interfaces for handling different shapes

--- structs_methods,interfaces/perimeter.go
+++ structs_methods,interfaces/perimeter.go
@@ -1,9 +1,32 @@
 package structs_methods_interfaces
 
-func Perimeter(width, height float64) float64 {
-	return 2 * (height + width)
+import "math"
+
+type Shape interface {
+	Area() float64
 }
 
-func Area(width, height float64) float64 {
-	return height * width
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Height + rectangle.Width)
+}
+
+func Area(rectangle Rectangle) float64 {
+	return rectangle.Height * rectangle.Width
 }
--- structs_methods,interfaces/perimeter_test.go
+++ structs_methods,interfaces/perimeter_test.go
@@ -5,7 +5,8 @@ import (
 )
 
 func TestPerimeter(t *testing.T) {
-	got := Perimeter(10.0, 10.0)
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
 	want := 40.0
 
 	if got != want {
@@ -14,9 +15,25 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	got := Area(10.0, 10.0)
-	want := 100.0
-	if got != want {
-		t.Errorf("got %.2f, want %.2f", got, want)
+
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+		if got != want {
+			t.Errorf("got %g, want %g", got, want)
+		}
 	}
+
+	t.Run("rectangles", func(t *testing.T) {
+
+		rectangle := Rectangle{10.0, 10.0}
+		want := 100.0
+		checkArea(t, rectangle, want)
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		want := 314.1592653589793
+		checkArea(t, circle, want)
+	})
 }
